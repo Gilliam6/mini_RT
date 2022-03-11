18209c04040940748fa743a5578f559f3def796d
rstephan <rstephan@student.21-school.ru>
2022-03-11 19:10:10 +0300

    Main struct added + mlx init + init new window with standart params

--- includes/mini_RT.h
+++ includes/mini_RT.h
@@ -7,5 +7,22 @@
 # include <stdlib.h>
 # include "../libft/libft.h"
 # include "../mlx/mlx.h"
+# define ARG_ERR			"Invalid number of arguments\n"
+# define WIN_SIZE_WIDTH		1024.0
+# define WIN_SIZE_HEIGHT	800.0
 
+typedef struct s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_img_data;
+
+typedef struct s_tracer
+{
+	void			*mlx;
+	void			*win;
+	t_img_data		*img;
+}				t_tracer;
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,7 +1,20 @@
-#include <stdio.h>
+#include "../includes/mini_RT.h"
 
-int main()
+int	unexpected_exit(const char *str)
 {
-	printf("Hello, World!\n");
-	return 0;
+	printf("%s", str);
+	return (1);
+}
+
+int main(int argc, char **argv)
+{
+	t_tracer	rt;
+	(void)argv;
+	if (argc != 2)
+		unexpected_exit(ARG_ERR);
+	rt.mlx = mlx_init();
+	rt.win = mlx_new_window(rt.mlx, WIN_SIZE_WIDTH, WIN_SIZE_HEIGHT, "mini_RT");
+	mlx_loop(rt.mlx);
+
+	return (0);
 }
