7e333815ba2e6c91f956bef2a264cbaa54f4c102
Gilliam6 <wizzzardoz@icloud.com>
2022-03-11 23:06:40 +0300

    structures done

--- includes/mini_RT.h
+++ includes/mini_RT.h
@@ -24,41 +24,80 @@ typedef struct s_data {
 	int		endian;
 }				t_img_data;
 
-typedef struct s_a_light
-{
+typedef struct s_color {
+	unsigned char R;
+	unsigned char G;
+	unsigned char B;
+}				t_color;
 
-};
+typedef struct s_coordinate {
+	double x;
+	double y;
+	double z;
+}				t_coord;
 
 typedef struct s_light
 {
-
-};
+	char		*indent;
+	t_coord		xyz;
+	double 		bright;
+	t_color		color;
+}				t_light;
 
 typedef struct s_camera
 {
+	char		*indent;
+	t_coord		xyz;
+	t_coord 	vector;
+	int			FOV;
+}				t_camera;
 
-};
+typedef struct s_ambient
+{
+	char		*indent;
+	t_color		color;
+	double		bright;
+}				t_ambient;
 
 typedef struct s_sphere
 {
-
-};
+	char		*indent;
+	t_color		color;
+	t_coord		xyz;
+	double		diameter;
+}				t_sphere;
 
 typedef struct s_plane
 {
-
-};
+	char		*indent;
+	t_color		color;
+	t_coord		xyz;
+	t_coord		vector;
+}				t_plane;
 
 typedef struct s_cylinder
 {
+	char		*indent;
+	t_color		color;
+	t_coord		xyz;
+	t_coord		vector;
+	double		height;
+	double		diameter;
+}				t_cylinder;
 
-};
+
 
 typedef struct s_tracer
 {
 	void			*mlx;
 	void			*win;
 	t_img_data		*img;
+	t_ambient		*ambient;
+	t_camera		*camera;
+	t_cylinder		*cyl;
+	t_light			*light;
+	t_plane			*plane;
+	t_sphere		*sphere;
 }				t_tracer;
 
 //HOOKS
