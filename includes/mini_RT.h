abbc4dcb90dbd4c2a3526a95709bc526e31fbebe
rstephan <rstephan@student.21-school.ru>
2022-03-11 17:52:36 +0300

    libft added

--- includes/mini_RT.h
+++ includes/mini_RT.h
@@ -1,4 +1,10 @@
 #ifndef MINI_RT_H
 # define MINI_RT_H
-
+# include <math.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include "../mlx/mlx.h"
+# include <stdlib.h>
+# include "../libft/libft.h"
 #endif
